78494156d47d804a7cd932da76fcdb4ca397c0db
Max <maxjbbrouwer@gmail.com>
2026-09-18 20:09:33 +0200

    Change Value to use record syntax

--- main.hs
+++ main.hs
@@ -1,37 +1,47 @@
--- Custom datatype to represent operations
-data Op 
-  = Add 
-  | Mul 
-  | Abs 
-  | Signum 
-  | Negate 
-  | None
+-- Datatype for operations on expression graph nodes
+data Op = Add | Mul 
   deriving (Eq, Show)
 
--- Expression graph node definition
+-- Datatype for expression graph nodes
 data Value 
-  = Leaf Float
-  | Node Float [Value] Op
+  = Leaf { val :: Float, grad :: Float, label :: Maybe String }
+  | Node { val :: Float, grad :: Float, children :: [Value], op :: Op, label :: Maybe String }
   deriving (Eq, Show)
 
--- Extract scalar Float value from any node
-val :: Value -> Float
-val (Leaf x)     = x
-val (Node x _ _) = x
+-- Constructors
+-- Default constructor for leaves/variables
+var :: Float -> String -> Value
+var x name = Leaf { val = x, grad = 0.0, label = Just name }
+
+-- Default constructor for literal values
+constant :: Float -> Value
+constant x = Leaf { val = x, grad = 0.0, label = Nothing }
+
+-- Default constructor for intermediate computation nodes
+node :: Float -> [Value] -> Op -> Value
+node v ch o = Node { val = v, grad = 0.0, children = ch, op = o, label = Nothing }
+
+
+-- Helper to tag intermediate nodes
+setLabel :: String -> Value -> Value
+setLabel lbl v = v { label = Just lbl }
+
 
 instance Num Value where
-  (+) a b = Node (val a + val b) [a, b] Add
-  (*) a b = Node (val a * val b) [a, b] Mul
+  (+) a b = node (val a + val b) [a, b] Add
+  (*) a b = node (val a * val b) [a, b] Mul
+  fromInteger n = constant (fromInteger n)
 
-  abs a        = Node (abs $ val a) [a] Abs
-  signum a     = Node (signum $ val a) [a] Signum
-  negate a     = Node (negate $ val a) [a] Negate
-  fromInteger n = Leaf (fromInteger n)
+  negate a = a * (-1)
+  abs _    = error "abs not implemented"
+  signum _ = error "signum not implemented"
+
+
 
 main :: IO ()
 main = do
-  let a = Leaf 1.0
-  let b = Leaf 2.0
-  let c = a + b
+  let a = var 1.0 "a"
+  let b = var 2.0 "b"
+  let c = setLabel "c" (a + b)
   
-  print c
+  print d
