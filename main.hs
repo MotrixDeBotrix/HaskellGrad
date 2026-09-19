ea4c801e19ff0f2aec01963f85e482d361462d0a
Max <maxjbbrouwer@gmail.com>
2026-09-19 12:48:53 +0200

    Implement gradient maps, topological sort and backprop

--- main.hs
+++ main.hs
@@ -1,35 +1,39 @@
--- Datatype for operations on expression graph nodes
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map)
+import Data.List (foldl')
+
+type GradMap = Map Value Float
+
+-- Datatype for operations
 data Op = Add | Mul | Tanh
-  deriving (Eq, Show)
+  deriving (Eq, Ord, Show)
 
--- Datatype for expression graph nodes
+-- Datatype for expression graph nodes (added Ord deriving)
 data Value 
-  = Leaf { val :: Float, grad :: Float, label :: Maybe String }
-  | Node { val :: Float, grad :: Float, children :: [Value], op :: Op, label :: Maybe String }
-  deriving (Eq, Show)
-
-tanh' :: Value -> Value
-tanh' v = node (tanh (val v)) [v] Tanh
-
+  = Leaf { val :: Float, label :: Maybe String }
+  | Node { val :: Float, children :: [Value], op :: Op, label :: Maybe String }
+  deriving (Eq, Ord, Show)
 
 -- Constructors
--- Default constructor for leaves/variables
 var :: Float -> String -> Value
-var x name = Leaf { val = x, grad = 0.0, label = Just name }
+var x name = Leaf { val = x, label = Just name }
 
--- Default constructor for literal values
 constant :: Float -> Value
-constant x = Leaf { val = x, grad = 0.0, label = Nothing }
+constant x = Leaf { val = x, label = Nothing }
 
--- Default constructor for intermediate computation nodes
 node :: Float -> [Value] -> Op -> Value
-node v ch o = Node { val = v, grad = 0.0, children = ch, op = o, label = Nothing }
+node v ch o = Node { val = v, children = ch, op = o, label = Nothing }
 
+-- Safe helper to extract children (returns [] for Leaf)
+getChildren :: Value -> [Value]
+getChildren (Node { children = ch }) = ch
+getChildren (Leaf {})                = []
 
--- Helper to tag intermediate nodes
 setLabel :: String -> Value -> Value
 setLabel lbl v = v { label = Just lbl }
 
+tanh' :: Value -> Value
+tanh' v = node (tanh (val v)) [v] Tanh
 
 instance Num Value where
   (+) a b = node (val a + val b) [a, b] Add
@@ -41,11 +45,58 @@ instance Num Value where
   signum _ = error "signum not implemented"
 
 
+-- Gradients
+getGrad :: Value -> GradMap -> Float
+getGrad v grads = Map.findWithDefault 0.0 v grads
+
+addGrad :: Value -> Float -> GradMap -> GradMap
+addGrad child delta gradMap = Map.insertWith (+) child delta gradMap
+
+-- Safely inspect operation and children
+propagateValue :: Value -> GradMap -> GradMap
+propagateValue v grads =
+  case v of
+    Node { op = Add, children = [a, b] } ->
+      addGrad a (g * 1.0) . addGrad b (g * 1.0) $ grads
+
+    Node { op = Mul, children = [a, b] } ->
+      addGrad a (g * val b) . addGrad b (g * val a) $ grads
+
+    Node { op = Tanh, children = [a] } ->
+      let y = val v
+      in addGrad a (g * (1.0 - y * y)) grads
+
+    _ -> grads
+  where
+    g = getGrad v grads
+
+-- Topological sort
+topoSort :: Value -> [Value]
+topoSort root = go [] root
+  where
+    go visited v 
+      | v `elem` visited = visited
+      | otherwise        = v : foldl' go visited (getChildren v)
+
+
+backprop :: Value -> GradMap
+backprop root =
+  let topo = topoSort root
+      initialGrads = Map.singleton root 1.0
+  in foldl' (\grads v -> propagateValue v grads) initialGrads topo
+
 
 main :: IO ()
 main = do
-  let a = var 1.0 "a"
-  let b = var 2.0 "b"
-  let c = setLabel "c" (a + b)
+  let a = var 2.0 "a"
+  let b = var 3.0 "b"
   
-  print $ tanh' c
+  -- d = (a * b) + b = (2 * 3) + 3 = 9.0
+  let d = (a * b) + b
+  let e = d * 2
+  
+  let grads = backprop e
+  
+  putStrLn $ "d = " ++ show (val d)
+  putStrLn $ "d/da = " ++ show (getGrad a grads)  -- Output: 3.0
+  putStrLn $ "d/db = " ++ show (getGrad b grads)  -- Output: 3.0
