f8b9f522b856a4e045ef2dc704fa6cc7f1082d1f
Max <maxjbbrouwer@gmail.com>
2026-09-19 13:00:16 +0200

    Implement pow and division (pow x -1)

--- main.hs
+++ main.hs
@@ -5,7 +5,7 @@ import Data.List (foldl')
 type GradMap = Map Value Float
 
 -- Datatype for operations
-data Op = Add | Mul | Tanh
+data Op = Add | Mul | Pow Float | Tanh
   deriving (Eq, Ord, Show)
 
 -- Datatype for expression graph nodes (added Ord deriving)
@@ -32,8 +32,12 @@ getChildren (Leaf {})                = []
 setLabel :: String -> Value -> Value
 setLabel lbl v = v { label = Just lbl }
 
+pow :: Value -> Float -> Value
+pow x k = node (val x ** k) [x] (Pow k)
+
 tanh' :: Value -> Value
 tanh' v = node (tanh (val v)) [v] Tanh
+
 
 instance Num Value where
   (+) a b = node (val a + val b) [a, b] Add
@@ -43,6 +47,10 @@ instance Num Value where
   negate a = a * (-1)
   abs _    = error "abs not implemented"
   signum _ = error "signum not implemented"
+
+instance Fractional Value where
+  recip x        = pow x (-1.0)
+  fromRational r = constant (fromRational r)
 
 
 -- Gradients
@@ -63,8 +71,11 @@ propagateValue v grads =
       addGrad a (g * val b) . addGrad b (g * val a) $ grads
 
     Node { op = Tanh, children = [a] } ->
-      let y = val v
-      in addGrad a (g * (1.0 - y * y)) grads
+      addGrad a (g * (1.0 - (val v)**2)) grads
+
+    Node { op = Pow k, children = [a] } ->
+      let localDeriv = k * (val a ** (k - 1.0))
+      in addGrad a (g * localDeriv) grads
 
     _ -> grads
   where
@@ -88,15 +99,14 @@ backprop root =
 
 main :: IO ()
 main = do
-  let a = var 2.0 "a"
-  let b = var 3.0 "b"
+  let a = var 6.0 "a"
+  let b = var 2.0 "b"
   
-  -- d = (a * b) + b = (2 * 3) + 3 = 9.0
-  let d = (a * b) + b
-  let e = d * 2
+  -- d = a / b = 6.0 / 2.0 = 3.0
+  let d = a / b
   
-  let grads = backprop e
+  let grads = backprop d
   
   putStrLn $ "d = " ++ show (val d)
-  putStrLn $ "d/da = " ++ show (getGrad a grads)  -- Output: 3.0
-  putStrLn $ "d/db = " ++ show (getGrad b grads)  -- Output: 3.0
+  putStrLn $ "d/da = " ++ show (getGrad a grads)
+  putStrLn $ "d/db = " ++ show (getGrad b grads)
